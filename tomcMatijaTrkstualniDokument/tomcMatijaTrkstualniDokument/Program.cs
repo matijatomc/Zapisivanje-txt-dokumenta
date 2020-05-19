9b05dc432d88a62c70831cd7647ad01d065f90c7
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-05-19 19:17:40 +0200

    Add files via upload

---
+++ tomcMatijaTrkstualniDokument/tomcMatijaTrkstualniDokument/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace tomcMatijaTrkstualniDokument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string folder = @"C:\Users\Matija\Documents\";
+            string fileName = "CSharpCornerAuthors.txt";
+            string fullPath = folder + fileName;
+
+            string[] authors = {"Mahesh Chand", "Allen O'Neill", "David McCarter","Raj Kumar", "Dhananjay Kumar"};
+            File.WriteAllLines(fullPath, authors);
+            string readText = File.ReadAllText(fullPath);
+            Console.WriteLine(readText);
+
+            Console.ReadKey();
+        }
+    }
+}
